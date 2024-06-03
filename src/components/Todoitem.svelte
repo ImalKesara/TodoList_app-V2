bd63780b54c391449305cbfb15858a534a41e3c8
LoneWolf <imalkesara2001@gmail.com>
2024-06-04 02:57:43 +0530

    item compoent

---
+++ src/components/Todoitem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import TodoitemActions from './TodoitemActions.svelte';
+</script>
+<div>
+    <span>Buy some donuts</span>
+    <TodoitemActions on:delete />
+</div>
+
+
+<style>
+    div{
+        display: flex;
+        padding: 20px;
+        border: 1px solid black;
+        justify-content: space-between;
+    }
+</style>
