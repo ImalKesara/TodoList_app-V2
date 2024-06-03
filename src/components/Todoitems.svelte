cdd666f5c662ed2c17ad5928e8d3c6ab013a94a4
LoneWolf <imalkesara2001@gmail.com>
2024-06-04 02:57:58 +0530

    todos

---
+++ src/components/Todoitems.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Todoitem from "./Todoitem.svelte";
+
+  const onDelete = () => {
+      alert('deleted successfully')
+  }
+
+</script>
+
+<div>
+  <Todoitem  on:delete = {onDelete}/>
+  
+</div>
+
+<style>
+  div{
+    display: flex;  
+    gap: 1em;
+    flex-direction: column;
+  }
+</style>
