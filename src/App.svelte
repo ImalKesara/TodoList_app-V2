c9c3237b600cd134da077b614b0663426bf11bf8
LoneWolf <imalkesara2001@gmail.com>
2024-06-06 02:26:47 +0530

    items array

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
+	import TodoitemCreate from './components/TodoitemCreate.svelte';
 	import Todoitems from './components/Todoitems.svelte';
+	import type { Task } from './model.ts';
+	
+
+	let items:Task[] =[
+    {
+      id : "1",
+      title : "Buy some donuts",
+      completed : false, 
+    },
+    {
+      id : "2",
+      title : "Buy some chocolut",
+      completed : false, 
+    }
+
+  ];
   
 </script>
 
 
 
 <div class="card">
- <Todoitems /> 
+ <Todoitems bind:items= {items} /> 
+ <TodoitemCreate bind:items = {items} />
 </div>
