7a3568dd2bfa673e953fa28b29c8b9b11221bb08
LoneWolf <imalkesara2001@gmail.com>
2024-06-06 02:27:42 +0530

    removed items array

--- src/components/Todoitems.svelte
+++ src/components/Todoitems.svelte
@@ -2,36 +2,23 @@
   import type { Task } from "../model";
   import Todoitem from "./Todoitem.svelte";
 
+  export let items:Task[];
+
   const onDelete = (item :Task) => {
       items = items.filter((i:Task) => i.id != item.id )
       alert('deleted successfully')
   }
   
 
-  let items:Task [] =[
-    {
-      id : "1",
-      title : "Buy some donuts",
-      completed : false, 
-    },
-    {
-      id : "2",
-      title : "Buy some chocolut",
-      completed : false, 
-    }
-
-  ]
-
   
-
 </script>
 
 <div>
   {#each items as item(item.id) }
-    <Todoitem bind:data = {item}  on:delete = {()=>onDelete(item)}/>  
+    <Todoitem  bind:data = {item}  on:delete = {()=>onDelete(item)}/>  
   {/each}
 
-  {JSON.stringify(items)}
+  <!-- {JSON.stringify(items)} -->
 
 </div>
 
