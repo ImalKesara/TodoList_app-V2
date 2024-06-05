029c5da4fced3103d060795db4592d1de6e42f49
LoneWolf <imalkesara2001@gmail.com>
2024-06-06 03:21:11 +0530

    import uuid lib

--- src/components/TodoitemCreate.svelte
+++ src/components/TodoitemCreate.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
 	import type { Task } from './../model.ts';
+    import { v4 as uuidv4 } from 'uuid';
     export let items:Task[];
+
+    let title : string = "";
+
+    const onKeydown = (e:KeyboardEvent)=>{
+        if(e.key != "Enter"){
+            return; // noting excute
+        }
+
+        items = [...items, {
+            id : uuidv4(),
+            title,
+            completed :false,
+        }]
+
+        items = items;
+
+        console.log(items)
+
+        title = "";        
+    }
 </script>
 
-<input type="text" placeholder="Create a new task">
+<input bind:value= {title} type="text" on:keydown={onKeydown} placeholder="Create a new task">
+
+<style>
+    input{
+        width: 100%;
+        padding: 1em;
+        box-sizing: border-box;
+    }
+</style>
