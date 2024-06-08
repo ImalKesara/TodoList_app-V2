fc15403b0a54850c85aad50be2c5771c83f4f436
LoneWolf <imalkesara2001@gmail.com>
2024-06-08 22:44:09 +0530

    visible and editble function

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
 	import TodoitemCreate from './components/items/TodoitemCreate.svelte';
 	import Todoitems from './components/items/Todoitems.svelte';
-  import EditTaskModal from './components/modal/EditTaskModal.svelte';
+  	import EditTaskModal from './components/modal/EditTaskModal.svelte';
 	import type { Task } from './model.ts';
+
 	
+	let editTaskModalData : Task | null  = null;
+	let EditTaskModalVisible = false;
 
 	let items:Task[] =[];
+
+	const onTaskEdit = (e)=>{
+		console.log(e.detail)
+	}
+
   
 </script>
 
 
 
 <div class="card">
-	<EditTaskModal />
-	<Todoitems bind:items= {items} /> 
-	<TodoitemCreate bind:items = {items} />
+	<!-- Edit pop menu -->
+	<EditTaskModal visible = {EditTaskModalVisible} data = {editTaskModalData} />
+	<!-- Todoitems -->
+	<Todoitems bind:items= {items} on:edit= {onTaskEdit}/> 
+	<!-- inputBox -->
+	<TodoitemCreate bind:items = {items} />  
 </div>
--- src/components/util/modal/Modal.svelte
+++ src/components/util/modal/Modal.svelte
@@ -22,9 +22,9 @@
             top: 0;
             width: 100%; /* Full width */
             height: 100%; /* Full height */
-            overflow: auto; /* Enable scroll if needed */
-            background-color: rgb(0,0,0); /* Fallback color */
-            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            overflow: auto; 
+            background-color: rgb(0,0,0); 
+            background-color: rgba(0,0,0,0.4); 
     }
 
     .modal-content {
