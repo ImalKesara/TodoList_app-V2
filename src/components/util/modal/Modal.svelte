52f64d0e5b13b4ff4b3300696357aae4d590e800
LoneWolf <imalkesara2001@gmail.com>
2024-06-08 22:10:58 +0530

    styles

---
+++ src/components/util/modal/Modal.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let visible;
+    function close(){
+        visible = false;
+    }
+
+    
+</script>
+
+<div class="modal-bg" on:click={close}>
+    <div class="modal-content" on:click|stopPropagation>
+        <slot />
+    </div>
+</div>
+
+<style>
+    .modal-bg {
+        
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+
+    .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+}
+</style>
