0f779e866a25d9a919c1274cd0f184d37a2c656b
LoneWolf <imalkesara2001@gmail.com>
2024-06-08 22:11:47 +0530

    import edittaskModal component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import TodoitemCreate from './components/items/TodoitemCreate.svelte';
 	import Todoitems from './components/items/Todoitems.svelte';
+  import EditTaskModal from './components/modal/EditTaskModal.svelte';
 	import type { Task } from './model.ts';
 	
 
@@ -11,6 +12,7 @@
 
 
 <div class="card">
+	<EditTaskModal />
 	<Todoitems bind:items= {items} /> 
 	<TodoitemCreate bind:items = {items} />
 </div>
