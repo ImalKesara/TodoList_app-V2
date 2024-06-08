ed060b5a33a80fb78e05758a463158465d707878
LoneWolf <imalkesara2001@gmail.com>
2024-06-08 22:11:24 +0530

    edit popup

---
+++ src/components/modal/EditTaskModal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Task } from '../../model';
+    import Modal from './../util/modal/Modal.svelte';
+
+  let visible :boolean = false;
+  export let data:Task;
+  
+  function close():void{
+    visible =false;
+  }
+  
+</script>
+
+
+{#if visible}
+
+    <Modal bind:visible = {visible}>
+        <div>
+            <span>Edit task </span>
+            <input type="text" bind:value={data.title}>
+            <button on:click={close}>close</button>
+        </div>
+</Modal>
+
+{/if}
+<style>
+    div{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
+
