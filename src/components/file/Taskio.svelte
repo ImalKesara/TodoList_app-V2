d98dd7bf7d1582ee6059eaced4214283f4007092
LoneWolf <imalkesara2001@gmail.com>
2024-06-09 23:44:04 +0530

    upload file from pc

--- src/components/file/Taskio.svelte
+++ src/components/file/Taskio.svelte
@@ -2,8 +2,8 @@
   import type { Task } from "../../model";
 
   export let items:Task[] =[];
-
   let filename:string = "";
+  let importFiles : FileList;
 
 
     function save(){
@@ -13,6 +13,31 @@
         a.download = filename;
         a.click();
     }
+
+    //read upload file from computer 
+    function onFilesChanged(files: FileList){
+        if(files == null){
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (e)=>{
+            try{
+                const result = e.target.result;
+                if(typeof result != "string")
+                    return;
+            
+                const data:Task[] = JSON.parse(result)
+                items = data;
+		    }catch(err){
+                console.log(err);
+            }
+        }
+        reader.readAsText(files[0])
+    }
+
+    
+
+    $:onFilesChanged(importFiles);
 </script>
 
 
@@ -20,7 +45,7 @@
     <input type="text" placeholder="file name">
     <button on:click={save}>Save file</button>
     <br>
-    <input type="file">
+    <input type="file" bind:files={importFiles}>
 </div>
 
 <style>
