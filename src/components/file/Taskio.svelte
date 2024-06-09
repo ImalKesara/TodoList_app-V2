c03f404505f2cdc652ed0abc43fe40d86b6a1866
LoneWolf <imalkesara2001@gmail.com>
2024-06-09 23:13:29 +0530

    task input output component

---
+++ src/components/file/Taskio.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { Task } from "../../model";
+
+  export let items:Task[] =[];
+
+  let filename:string = "";
+
+
+    function save(){
+        const string = JSON.stringify(items);
+        const a = document.createElement("a");
+        a.href = `data:application/json;charset=utf-8,${string}`;
+        a.download = filename;
+        a.click();
+    }
+</script>
+
+
+<div>
+    <input type="text" placeholder="file name">
+    <button on:click={save}>Save file</button>
+    <br>
+    <input type="file">
+</div>
+
+<style>
+    div{
+        margin-top: 0.5em;
+
+    }
+</style>
