12d698a020583abb2710652b17381a41e0471461
LoneWolf <imalkesara2001@gmail.com>
2024-06-12 14:51:21 +0530

    applied round border

--- src/components/util/modal/Modal.svelte
+++ src/components/util/modal/Modal.svelte
@@ -13,7 +13,6 @@
 
 <style>
     .modal-bg {
-        
             position: fixed; /* Stay in place */
             z-index: 1; /* Sit on top */
             left: 0;
@@ -26,10 +25,11 @@
     }
 
     .modal-content {
+        @apply rounded-xl ;
         background-color: #fefefe;
         margin: 15% auto; /* 15% from the top and centered */
         padding: 20px;
         border: 1px solid #888;
         width: 80%; /* Could be more or less, depending on screen size */
-}
+    }
 </style>
