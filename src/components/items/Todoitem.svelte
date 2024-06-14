089da3269bb3151a200f279b45cf986a8cff6fc3
LoneWolf <imalkesara2001@gmail.com>
2024-06-14 22:40:08 +0530

    line through for completed task

--- src/components/items/Todoitem.svelte
+++ src/components/items/Todoitem.svelte
@@ -11,7 +11,7 @@
 
 
 <div class="bg-violet-300 rounded-full border-none p-4 shadow-lg text-transform: capitalize font-medium">
-    <span spellcheck="false" contenteditable="true" bind:textContent={data.title} on:blur={onTitleBlur}></span>
+    <span  class:line-through= {data.completed} class="outline-none" spellcheck="false" contenteditable="true" bind:textContent={data.title} on:blur={onTitleBlur}></span>
     <TodoitemActions on:delete on:edit  bind:completed = {data.completed}  on:completedChange />    
 </div>
 
